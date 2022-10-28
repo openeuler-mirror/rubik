6704b49514419688a38964e9758eeee4e2e793b4
lukeAyin <lukeayin@gmail.com>
2022-10-28 22:17:52 +0800

    opensummer:delete redundant file

--- tests/tools/interferenceanalysis/__init__.py
+++
@@ -1,4 +0,0 @@
-from .base import BaseClass, base_function
-from .preprocess import Preprocess, StressProcess, MachineProcess
-
-__all__ = ["BaseClass", "base_function", "Preprocess", "StressProcess", "MachineProcess"]
