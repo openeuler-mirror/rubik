70153eacc28c1ca06e766f095863b12452796f4a
DCCooper <1866858@gmail.com>
2022-12-08 10:12:29 +0800

    refactor: move implicit import to import.go file
    Signed-off-by: DCCooper <1866858@gmail.com>

--- pkg/rubik/rubik.go
+++ pkg/rubik/rubik.go
@@ -2,14 +2,6 @@ package rubik
 
 import (
 	"fmt"
-
-	// import packages to auto register service
-	_ "isula.org/rubik/pkg/modules/blkio"
-	_ "isula.org/rubik/pkg/modules/cachelimit"
-	_ "isula.org/rubik/pkg/modules/cpu"
-	_ "isula.org/rubik/pkg/modules/iocost"
-	_ "isula.org/rubik/pkg/modules/memory"
-	_ "isula.org/rubik/pkg/version"
 )
 
 func Run() int {
